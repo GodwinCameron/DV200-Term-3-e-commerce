a332c7b22e65506cfcdf805253e039b8b113a821
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2022-08-10 11:08:31 +0300

    Outlet added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './main.css';
 import { Route, Routes } from 'react-router-dom';
 import Landing from './components/Landing';
 import IndividualProduct from './components/IndividualProduct';
+import { RouterSetup } from './components/RouterSetup';
 
 
 
@@ -16,6 +17,7 @@ function App() {
       <Route path='/Login' element={<Login/>}/>
       <Route path='/IndividualProduct' element={<IndividualProduct/>}/>
     </Routes>
+    <RouterSetup/>
     </>
   );
 }
